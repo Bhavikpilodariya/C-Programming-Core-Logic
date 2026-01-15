dbb99d167e8973024967b9d18a1584206ef10f23
Bhavikpilodariya <148178634+Bhavikpilodariya@users.noreply.github.com>
2026-01-15 11:32:52 +0530

    Create area_of_traingle.c

---
+++ area_of_traingle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+ {
+     int area;
+     int height;
+     int base;
+     
+     printf("Entere the value of height");
+     scanf("%d",&height);
+     printf("Entere the value of base");
+     scanf("%d",&base);
+     
+     area=(base*height)/2;
+     printf("%d",area);
+     
+     return 0;
+     
+ }
