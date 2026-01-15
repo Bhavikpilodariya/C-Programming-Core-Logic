8d5f2e7085e748b9ebb9154dd0a90b4806f25782
Bhavikpilodariya <148178634+Bhavikpilodariya@users.noreply.github.com>
2026-01-15 11:44:27 +0530

    Create find_ditance.c

---
+++ find_ditance.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int d;
+    int u;
+    int a;
+    int t;
+    printf("Entere the value of  a");
+    scanf("%d",&a);
+        printf("Entere the value of  u");
+    scanf("%d",&u);
+    printf("Entere the value of  t");
+    scanf("%d",&t);
+    
+    d=(u*t) +(a*t*t);
+    printf("%d",d);
+    
+    return 0;
+
+
+    
+}
